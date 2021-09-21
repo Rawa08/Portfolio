e875b3c8b8a3d3f86ba27ea29c582d6d2d39fdc1
Rawa Aref <rawa_aref@hotmail.com>
2021-09-21 16:42:46 +0200

    mail configuration

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const next = require('next');
-const bodyParser = require('body-parser');
+
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -9,7 +9,10 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = express()
 
-  server.use(bodyParser.json())
+  server.use(express.json())
+  server.use(express.urlencoded({ extended: true }))
+
+  
 
   server.get('*', (req, res) => {
     return handle(req, res)
