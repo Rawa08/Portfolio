5b8f11b1e1a6e3cf82e212d8164365f4ab0c0ed2
Rawa Aref <rawa_aref@hotmail.com>
2021-09-22 16:27:20 +0200

    changing to EmailJs

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,40 +1,27 @@
 import { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import styles from '../styles/Home.module.css';
+import emailjs from 'emailjs-com';
+
 
 
 const ContactForm = () => {
-  const [subject, setSubject] = useState('');
-  const [text, setText] = useState('');
-  const [from, setFrom] = useState('');
+
   const [feedback, setFeedback] = useState({ success: false, text: '', alertVariant: '',notSuccess:false });
+
 
   const submitForm = (e) => {
     e.preventDefault();
 
-    fetch('/api/contact', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        fromMail: from,
-        subject: subject,
-        message: text
-      })
-    }).then((res) => {
-      console.log(res.status)
-      if (res.status === 200) {
-        setFrom('');
-        setSubject('');
-        setText('');
+    emailjs.sendForm('service_vgumcc9','template_qvnrocc', e.target, 'user_bmodrfQbdflZTqZbhG5xt')
+      .then((result) => {
         setFeedback({ success: true ,text: 'Your message have been sent, i will get back to you asap', alertVariant: 'info' })
-      } else {
+          console.log(result.text);
+      }, (error) => {
         setFeedback({ success: false, text: 'Something went wrong please try again', alertVariant: 'danger' , notSuccess:true})
-      }
-    })
+      });
+      e.target.reset()
   }
-
 
 
   return (
@@ -48,20 +35,20 @@ const ContactForm = () => {
       <Form onSubmit={e => submitForm(e)} className="m-2">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Your email:</Form.Label>
-          <Form.Control type="email" value={from} onChange={e => setFrom(e.target.value)} required />
-          <Form.Text className="text-muted" onChange={e => setText(e.target.value)} />
+          <Form.Control type="email" name="email" required />
+         
 
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Subject:</Form.Label>
-          <Form.Control type="text" value={subject} onChange={e => setSubject(e.target.value)} required />
+          <Form.Control type="text" name="subject" required />
         </Form.Group>
 
 
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Your Message:</Form.Label>
-          <Form.Control as="textarea" rows={3} value={text} onChange={e => setText(e.target.value)} required />
+          <Form.Control as="textarea" rows={3} name="message" required />
         </Form.Group>
         <Button className={styles.formButton} type="submit">
           Submit
