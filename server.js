73f24036a9ec540d0ce2e78ca0b754ded6858fc9
Rawa Aref <rawa_aref@hotmail.com>
2021-09-22 13:40:35 +0200

    Testing mail client

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const next = require('next');
-const mailer = require('./mailConfig');
+const sgMail = require('@sendgrid/mail')
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const app = next({
+  dev
+})
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   const server = express()
 
   server.use(express.json())
-  server.use(express.urlencoded({ extended: true }))
+  server.use(express.urlencoded({
+    extended: true
+  }))
 
-  
+
 
   server.get('*', (req, res) => {
     return handle(req, res)
@@ -24,23 +28,36 @@ app.prepare().then(() => {
     console.log('> Read on http://localhost:5000')
   })
 
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
   server.post('/api/contact', (req, res) => {
-    const {fromMail, subject1, message1 } = req.body;
+    const {
+      fromMail,
+      subject,
+      message
+    } = req.body;
+
+    const msg = {
+      to: process.env.toMail, // Change to your recipient
+      from: fromMail, // Change to your verified sender
+      subject: subject,
+      text: message,
+
+    }
 
 
-    mailer({ fromMail, subject1, message1 }).then(() => {
-      console.log('success')
-  
-    }).catch((error) => {
-      console.log('failed', error)
- 
-    })
+    sgMail
+      .send(msg)
+      .then(() => {
+        res.send('success')
+      })
+      .catch((error) => {
+        res.status(500).send('error')
+        console.error(error)
+      
+      })
 
 
-    console.log(req.body)
-    res.send('success')
   })
 })
-
-
-
